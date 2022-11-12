97f419ba69e013cd9678504b37baa6d8a0c64c4a
anahi <rguez19@gmail.com>
2022-11-12 15:37:39 -0700

    Actividad2

--- index.php
+++ index.php
@@ -1,9 +1,44 @@
-<?php
-include('layouts/header.php');
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="images/images1.jpg" type="image/x-icon">
+    <title> Elotitos </title>
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
 
 
+<header>
 
+    <input type="checkbox" name="" id="toggler">
+    <label for="toggler" class="fas fa-bars"></label>
+
+    <a href="#" class="logo">Granjita<span>.</span></a>
+
+    <nav class="navbar">
+        <a href="#home">Inicio</a>
+        <a href="#about">Sobre Nostros</a>
+        <a href="#products">Productos</a>
+        <a href="#review">Opiniones</a>
+        <a href="#contact">Contactos</a>
+    </nav>
+
+    <div class="icons">
+        <a href="#" class="fas fa-heart"></a>
+        <a href="#" class="fas fa-shopping-cart"></a>
+        <a href="#" class="fas fa-user"></a>
+    </div>
+
+</header>
 
 
 <!-- header section starts  -->
@@ -368,9 +403,46 @@ include('layouts/header.php');
 
 <!-- footer section starts  -->
 
+<section class="footer">
 
-<?php
-include('layouts/footer.php');
-?>
+    <div class="box-container">
 
+        <div class="box">
+            <h3>links extras</h3>
+            <a href="#">Inicio</a>
+            <a href="#">Sobre Nosotros</a>
+            <a href="#">Productos</a>
+            <a href="#">Opiniones</a>
+            <a href="#">Contactos</a>
+        </div>
+
+        <div class="box">
+            <h3>Links</h3>
+            <a href="#">Mi cuenta</a>
+            <a href="#">Mi orden</a>
+            <a href="#">Mi favorito</a>
+        </div>
+
+        <div class="box">
+            <h3>Ubicaciones</h3>
+            <a href="#">Rosario</a>
+            <a href="#">Mazatlan</a>
+            <a href="#">Escuinapa</a>
+            <a href="#">Tepic</a>
+        </div>
+
+        <div class="box">
+            <h3>Contacto Directo</h3>
+            <a href="#">694-164-4109</a>
+            <a href="#">elotitos19@gmail.com</a>
+            <a href="#">Rosario,Sinaloa </a>
+        </div>
+
+    </div>
+
+</section>
+
+
+</body>
+</html>
 
